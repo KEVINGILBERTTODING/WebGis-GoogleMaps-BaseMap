b97b399248e5d5cfb33af7f056e2234b9c532b38
Kevin Gilbert Toding <122202002832@mhs.dinus.ac.id>
2021-12-09 00:00:53 +0800

    delete font awesome linkrel

--- header.php
+++ header.php
@@ -11,6 +11,7 @@ $obj = new Db_Class();
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css" />
+
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <style>
     /* Add a gray background color and some padding to the footer */
