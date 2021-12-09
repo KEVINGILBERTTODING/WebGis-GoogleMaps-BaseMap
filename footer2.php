6413210f27fc60e4865084c3af569a72b011f8a2
Kevin Gilbert Toding <122202002832@mhs.dinus.ac.id>
2021-12-10 06:16:44 +0800

    add footer 2

---
+++ footer2.php
@@ -0,0 +1,48 @@
+ <!--============================================================================8. Footer==========================================================-->
+ <div class="container">
+   <hr class="my-5">
+   <div class="row mb-5">
+
+     <div class="col-lg-3">
+       <a href="#">
+         <img src="../assets/img/semarang.png" height="50px">
+       </a>
+       <h6><br></h6>
+     </div>
+     <div class="col-lg-3">
+       <h5>Quick Links</h5>
+       <br><br>
+       <a href="#" class="link-navbar">About Us</a><br>
+       <a href="#" class="link-navbar">Blog</a><br>
+       <a href="#" class="link-navbar">Contact</a><br>
+       <a href="#" class="link-navbar">FAQ</a><br>
+       <br>
+     </div>
+     <div class="col-lg-3">
+       <h5>Legal Stuff</h5>
+       <br><br>
+       <a href="#" class="link-navbar">Disclaimer</a><br>
+       <a href="#" class="link-navbar">Financing</a><br>
+       <a href="#" class="link-navbar">Privacy Policy</a><br>
+       <a href="#" class="link-navbar">Terms of Service</a><br>
+       <br>
+     </div>
+     <div class="col-lg-3">
+       <h6>2021 <b>Kevin Gilbert Toding</b>. Made with ❤️ using Postgis and Geoserver.</h5>
+         <br>
+         <form action="#" method="post">
+           <div class="form-group">
+             <input type="text" class="form-control" placeholder="Masukkan no Handphone anda" required>
+           </div>
+           <br>
+           <button type="submit" class="btn btn-custom rounded shadow">Daftar Sekarang</button>
+         </form>
+     </div>
+
+   </div>
+ </div>
+
+ </body>
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+ </html>
